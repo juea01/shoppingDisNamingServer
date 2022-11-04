e073f2a50b540552f7ef32909cd96c70ae270ed3
juea01 <akashwarkalay@gmail.com>
2022-11-04 14:13:28 +1300

    Naming Server Microservices for Shopping District Web App

---
+++ src/test/java/com/shoppingdistrict/microservices/shoppingdistrictnamingserver/ShoppingdistrictNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingdistrict.microservices.shoppingdistrictnamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingdistrictNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
